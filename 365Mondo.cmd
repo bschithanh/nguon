523fc985da74d631b89aafadca7b07c39b83bdc4
bschithanh <driverbsthanh@gmail.com>
2025-03-24 11:57:03 +0700

    Update 365Mondo.cmd

--- 365Mondo.cmd
+++ 365Mondo.cmd
@@ -1,5 +1,5 @@
 @echo off
-title  Activate Microsoft Office Prolus 2016 for FREE - https://github.com/BsChiThanh 
+title  Activate Microsoft Mondo 2016 for FREE - https://github.com/BsChiThanh 
 cls
 color F4
 mode con cols=98 lines=30
