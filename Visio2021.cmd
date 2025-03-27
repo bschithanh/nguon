3e177721f502f95f662f9958dc5dfb2bf613ebc6
bschithanh <driverbsthanh@gmail.com>
2025-03-27 08:02:18 +0700

    Add files via upload

---
+++ Visio2021.cmd
@@ -0,0 +1,28 @@
+@echo off
+title  Activate Microsoft Office Visio 2021 for FREE - https://github.com/BsChiThanh 
+cls
+color F4
+mode con cols=98 lines=30
+ 
+if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16"
+if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+
+cscript ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\VisioProVL_KMS_Client-ppd.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\VisioProVL_KMS_Client-ul.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\VisioProVL_KMS_Client-ul-oob.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\VisioPro2021VL_KMS_Client_AE-ppd.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\VisioPro2021VL_KMS_Client_AE-ul.xrm-ms"
+cscript ospp.vbs /inslic:"..\root\Licenses16\VisioPro2021VL_KMS_Client_AE-ul-oob.xrm-ms"
+
+
+cscript //nologo ospp.vbs /setprt:1688
+cscript //nologo ospp.vbs /sethst:192.168.2.81.2.7.0
+cscript //nologo ospp.vbs /sethst:122.226.152.230
+cscript //nologo ospp.vbs /inpkey:KNH8D-FGHT4-T8RK3-CTDYJ-K2HT4
+cscript //nologo ospp.vbs /inpkey:MJVNY-BYWPY-CWV6J-2RKRT-4M8QG
+
+:end
+:notsupported
+:halt
+pause >nul
