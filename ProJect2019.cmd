e971a659fe2ea5a6f0d83dd1b917664d707547d9
bschithanh <driverbsthanh@gmail.com>
2025-03-28 13:19:03 +0700

    Update ProJect2019.cmd

--- ProJect2019.cmd
+++ ProJect2019.cmd
@@ -2,11 +2,9 @@
 title  Activate Microsoft Office Projet 2019 for FREE - https://github.com/BsChiThanh 
 cls
 color F4
-mode con cols=98 lines=30
- 
+mode con cols=98 lines=30 
 if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16"
 if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
-
 cscript ospp.vbs /inslic:"..\root\Licenses16\pkeyconfig-office.xrm-ms"
 cscript ospp.vbs /inslic:"..\root\Licenses16\ProjectProVL_KMS_Client-ppd.xrm-ms"
 cscript ospp.vbs /inslic:"..\root\Licenses16\ProjectProVL_KMS_Client-ul.xrm-ms"
@@ -14,14 +12,10 @@ cscript ospp.vbs /inslic:"..\root\Licenses16\ProjectProVL_KMS_Client-ul-oob.xrm-
 cscript ospp.vbs /inslic:"..\root\Licenses16\ProjectPro2019VL_KMS_Client_AE-ul.xrm-ms"
 cscript ospp.vbs /inslic:"..\root\Licenses16\ProjectPro2019VL_KMS_Client_AE-ul-oob.xrm-ms"
 cscript ospp.vbs /inslic:"..\root\Licenses16\ProjectPro2019VL_KMS_Client_AE-ppd.xrm-ms"
-
-
 cscript //nologo ospp.vbs /setprt:1688
 cscript //nologo ospp.vbs /sethst:192.168.2.81.2.7.0
 cscript //nologo ospp.vbs /sethst:122.226.152.230
 cscript //nologo ospp.vbs /inpkey:B4NPR-3FKK7-T2MBV-FRQ4W-PKD2B
-cscript //nologo ospp.vbs /inpkey:C4F7P-NCP8C-6CQPT-MQHV9-JXD2M
-
 :end
 :notsupported
 :halt
